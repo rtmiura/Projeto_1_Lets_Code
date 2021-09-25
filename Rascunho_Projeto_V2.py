913fac97f9912ff7090c5067ce9aaa5b8419cf71
rtmiura <rodrigotksh@gmail.com>
2021-09-25 16:44:20 -0300

    Update Rascunho_Projeto_V2.py
    Utilização de objeto para representar o jogador e gravar vitorias e derrotas

--- Rascunho_Projeto_V2.py
+++ Rascunho_Projeto_V2.py
@@ -36,6 +36,8 @@ def tratamento_letra(letra,Palavra_secreta):
     if letra.upper()=='Ç':
         if not('Ç' in Palavra_secreta.upper()):
             return letra.upper().strip()
+        else:
+            return tratamento(letra)
     else:
         return tratamento(letra)
 
@@ -124,132 +126,190 @@ def imprime_mensagem_vencedor():
     print("         .'   '.        ")
     print("        '-------'       ")
     
-    
-"""
-Definição caracteristicas do jogo
 
-"""
-Numero_tentativas=7
-
-"""
-#Elemento 1 Leitura ou sorteio da palavra a ser descoberta:
-
-"""
-
-arquivo = open('Lista_Teste.txt', 'r', encoding="utf-8")
-conteudo = arquivo.read()
-    
-arquivo.close()
-conteudo_lista=conteudo.split('\n')
-
-
-palavra_secreta=sorteio_palavra(conteudo_lista)
-# palavra_secreta='abc'
-# palavra_secreta='agua-viva'
-
-
-"""
-#Elemento 2 Tratamento da palavra secreta:
-
-"""
-
-Palavra_formatada=tratamento(palavra_secreta)
-#Palavra='Água'
-
-"""
-#Elemento 3 esconder a palavra tratada para mostrar para o jogador:
-
-"""
-
-Palavra_Display,numero_letras=esconde_palavra(Palavra_formatada)
-
-"""
-#Elemento 4 print da palavra escondida para o jogador:
-
-"""
-
-print(f'Atenção! A palavra secreta tem {numero_letras} letras')
-
-
-
-
-Letras_digitadas=[]
-
-
-while Numero_tentativas>0:
-    
-    print(f'Palavra secreta: {Palavra_Display}',end='')
-    
-    """
-    Elemento 5 leitura da tentativa do jogador:
-
-    """
+class Jogador(object):
+    def __init__(self,nome):
+        self.name=nome
+        self.vitorias=0
+        self.derrotas=0
+        self.n_tentativas_para_vencer=0
         
-    Novaletra=input('Digite o teu palpite, uma letra da palavra secreta: ')
-    if not validade_letra_jogador(Novaletra):
-        continue
+    def tenta_nova_letra(self):
+        return input('Digite o teu palpite, uma letra da palavra secreta: ')
         
-    Novaletra=tratamento_letra(Novaletra,palavra_secreta)
+    def retorna_nome_jogador(self):
+        return self.name
+    
+    def retorna_n_vitorias(self):
+        return self.vitorias
+    
+    def retorna_media_tentativas_para_ganhar(self):
+        return self.n_tentativas_para_vencer/self.vitorias
+    
+    def atualiza_vitoria(self):
+         self.vitorias+=1
+         
+    def atualiza_derrota(self):
+         self.derrotas+=1
+         
+    def atualiza_n_tentativas_para_vencer(self,tentativas):
+         self.n_tentativas_para_vencer+=tentativas
+    
+    
+    def __str__(self):
+        if self.vitorias>0:
+            return (f'Nome: {self.name} - Vitórias: {self.vitorias} - Derrotas: {self.derrotas} - Média de tentativa para ganhar {self.n_tentativas_para_vencer/self.vitorias}')
+        else:
+            return (f'Nome: {self.name} - Vitórias: {self.vitorias} - Derrotas: {self.derrotas}')
+    
+
+
+
+dic_jogadores={}
+
+Restart_jogo=False
+
+while not Restart_jogo:
+    
+    Nome_jogador=input('Escreva o Nome do jogador da rodada: ')
+    
+    if Nome_jogador not in dic_jogadores:
+        dic_jogadores[Nome_jogador]=Jogador(Nome_jogador)
     
     """
-    #Elemento 6 listando letras já digitadas:
-
+    Definição caracteristicas do jogo
+    
     """
-
-    if Novaletra in Letras_digitadas:
-        print(f'A letra {Novaletra} já foi digitada!')
-        if len(Letras_digitadas)>0:
-            print(f'Você já digitou este conjunto de letras: {Letras_digitadas}')
-        continue
-    else:
-        Letras_digitadas.append(Novaletra)
+    Numero_tentativas=7
+    
+    """
+    #Elemento 1 Leitura ou sorteio da palavra a ser descoberta:
+    
+    """
+    
+    arquivo = open('Lista_Teste.txt', 'r', encoding="utf-8")
+    conteudo = arquivo.read()
         
+    arquivo.close()
+    conteudo_lista=conteudo.split('\n')
+    
+    
+    palavra_secreta=sorteio_palavra(conteudo_lista)
+    # palavra_secreta='abc'
+    # palavra_secreta='agua-viva'
+    # palavra_secreta='ranço'
+    
     
     """
-    Elemento 7 Verificação se a palavra secreta contem a
-    letra da tentativa do jogador
-
-    """
-    
-    Vetor=[letra==Novaletra for letra in Palavra_formatada]
-    
-    
-    """
-    Elemento 8 Checar se o jogador acertou ou errou
-    
-    Elemento 9 Se acertou atualizar a palavra escondida com a letra
-    Checar se jogador completou a palavra e ganhou    
-    
-    Elemento 10 Se errou incrementar o numero de erros
-    Atualizar desenho da forca
-    Checar se jogador perdeu
+    #Elemento 2 Tratamento da palavra secreta:
     
     """
     
-    Palavra_Display_list=list(Palavra_Display)    
-
-
-    if not True in Vetor: #jogador Errou
-        Numero_tentativas-=1 #remove tentativa
-        print(f'A palavra secreta não contém a letra {Novaletra}!')
-        desenho_erros(Numero_tentativas) #desenha forca
-
-        if Numero_tentativas<1: #checa se perdeu
-            print(f'Perdeu a palavra era {palavra_secreta.upper()}!')
-        else: #mostra o numero de tentaticas restantes
-            print(f'Você pode cometer mais {Numero_tentativas} erros')
+    Palavra_formatada=tratamento(palavra_secreta)
+    #Palavra='Água'
     
-    else: #jogador Acertou
-        print(f'A palavra secreta contém a letra {Novaletra}!')
-        for i in range(len(Palavra_Display)): #Atualiza a letra na palavra escondida
-            if Vetor[i]:
-                Palavra_Display_list[i]=Novaletra
-        Palavra_Display=''.join(Palavra_Display_list)
+    """
+    #Elemento 3 esconder a palavra tratada para mostrar para o jogador:
+    
+    """
+    
+    Palavra_Display,numero_letras=esconde_palavra(Palavra_formatada)
+    
+    """
+    #Elemento 4 print da palavra escondida para o jogador:
+    
+    """
+    
+    print(f'Atenção! A palavra secreta tem {numero_letras} letras')
+    Letras_digitadas=[]
+
+
+    while Numero_tentativas>0:
         
+        print(f'Palavra secreta: {Palavra_Display}',end='')
+        
+        """
+        Elemento 5 leitura da tentativa do jogador:
     
-    
-    if Palavra_Display==Palavra_formatada: #checa se ganhou
-     print(f'Ganhou! A palavra secreta é {palavra_secreta.upper()}!')
-     imprime_mensagem_vencedor()
-     break
+        """
             
+        # Novaletra=input('Digite o teu palpite, uma letra da palavra secreta: ')
+        Novaletra=dic_jogadores[Nome_jogador].tenta_nova_letra()
+        
+        
+        if not validade_letra_jogador(Novaletra):
+            continue
+            
+        Novaletra=tratamento_letra(Novaletra,palavra_secreta)
+        
+        """
+        #Elemento 6 listando letras já digitadas:
+    
+        """
+    
+        if Novaletra in Letras_digitadas:
+            print(f'A letra {Novaletra} já foi digitada!')
+            if len(Letras_digitadas)>0:
+                print(f'Você já digitou este conjunto de letras: {Letras_digitadas}')
+            continue
+        else:
+            Letras_digitadas.append(Novaletra)
+            
+        
+        """
+        Elemento 7 Verificação se a palavra secreta contem a
+        letra da tentativa do jogador
+    
+        """
+        
+        Vetor=[letra==Novaletra for letra in Palavra_formatada]
+        
+        
+        """
+        Elemento 8 Checar se o jogador acertou ou errou
+        
+        Elemento 9 Se acertou atualizar a palavra escondida com a letra
+        Checar se jogador completou a palavra e ganhou    
+        
+        Elemento 10 Se errou incrementar o numero de erros
+        Atualizar desenho da forca
+        Checar se jogador perdeu
+        
+        """
+        
+        Palavra_Display_list=list(Palavra_Display)    
+    
+    
+        if not True in Vetor: #jogador Errou
+            Numero_tentativas-=1 #remove tentativa
+            print(f'A palavra secreta não contém a letra {Novaletra}!')
+            desenho_erros(Numero_tentativas) #desenha forca
+    
+            if Numero_tentativas<1: #checa se perdeu
+                print(f'Perdeu a palavra era {palavra_secreta.upper()}!')
+                dic_jogadores[Nome_jogador].atualiza_derrota()
+            else: #mostra o numero de tentaticas restantes
+                print(f'Você pode cometer mais {Numero_tentativas} erros')
+        
+        else: #jogador Acertou
+            print(f'A palavra secreta contém a letra {Novaletra}!')
+            for i in range(len(Palavra_Display)): #Atualiza a letra na palavra escondida
+                if Vetor[i]:
+                    Palavra_Display_list[i]=Novaletra
+            Palavra_Display=''.join(Palavra_Display_list)
+            
+        
+        
+        if Palavra_Display==Palavra_formatada: #checa se ganhou
+         print(f'Ganhou! A palavra secreta é {palavra_secreta.upper()}!')
+         imprime_mensagem_vencedor()
+         dic_jogadores[Nome_jogador].atualiza_vitoria()
+         dic_jogadores[Nome_jogador].atualiza_n_tentativas_para_vencer(len(Letras_digitadas))
+         break
+    
+    Restart_jogo = int(input('Quer continuar a jogar? Sim = 1, Não = 0 -'))!=1     
+            
+
+
+for jogador in dic_jogadores.values():
+    print(jogador)
