66b780fa7152f4bf349b90db7cd54a532ae2c28d
rtmiura <rodrigotksh@gmail.com>
2021-09-28 11:42:50 -0300

    Update Rascunho_Projeto_V2.py
    Adicionada uma variavél para separar a comparação da palavra secreta a a palavra semi-escondida. Objetivo já mostrar as letras escondidas com acentuação, ou outros caracteres especiais
    
    Adicionado ranking

--- Rascunho_Projeto_V2.py
+++ Rascunho_Projeto_V2.py
@@ -143,6 +143,9 @@ class Jogador(object):
     def retorna_n_vitorias(self):
         return self.vitorias
     
+    def retorna_n_derrotas(self):
+        return self.derrotas
+    
     def retorna_media_tentativas_para_ganhar(self):
         return self.n_tentativas_para_vencer/self.vitorias
     
@@ -162,6 +165,14 @@ class Jogador(object):
         else:
             return (f'Nome: {self.name} - Vitórias: {self.vitorias} - Derrotas: {self.derrotas}')
     
+
+
+
+
+arquivo = open('Lista_Teste.txt', 'r', encoding="utf-8")
+conteudo = arquivo.read()    
+arquivo.close()
+conteudo_lista=conteudo.split('\n')
 
 
 
@@ -187,19 +198,13 @@ while not Restart_jogo:
     
     """
     
-    arquivo = open('Lista_Teste.txt', 'r', encoding="utf-8")
-    conteudo = arquivo.read()
-        
-    arquivo.close()
-    conteudo_lista=conteudo.split('\n')
-    
-    
     palavra_secreta=sorteio_palavra(conteudo_lista)
+
     # palavra_secreta='abc'
-    # palavra_secreta='agua-viva'
-    # palavra_secreta='ranço'
+    # palavra_secreta='água-viva'
+    # palavra_secreta='rancaoc'
     
-    
+    palavra_secreta_list=list(palavra_secreta)
     """
     #Elemento 2 Tratamento da palavra secreta:
     
@@ -213,8 +218,8 @@ while not Restart_jogo:
     
     """
     
-    Palavra_Display,numero_letras=esconde_palavra(Palavra_formatada)
-    
+    Palavra_Comparar,numero_letras=esconde_palavra(Palavra_formatada)
+    Palavra_Display=Palavra_Comparar
     """
     #Elemento 4 print da palavra escondida para o jogador:
     
@@ -277,8 +282,8 @@ while not Restart_jogo:
         
         """
         
-        Palavra_Display_list=list(Palavra_Display)    
-    
+        Palavra_Comparar_list=list(Palavra_Comparar)    
+        Palavra_Display_list=list(Palavra_Display)
     
         if not True in Vetor: #jogador Errou
             Numero_tentativas-=1 #remove tentativa
@@ -293,14 +298,16 @@ while not Restart_jogo:
         
         else: #jogador Acertou
             print(f'A palavra secreta contém a letra {Novaletra}!')
-            for i in range(len(Palavra_Display)): #Atualiza a letra na palavra escondida
+            for i in range(len(Palavra_Comparar)): #Atualiza a letra na palavra escondida
                 if Vetor[i]:
-                    Palavra_Display_list[i]=Novaletra
+                    Palavra_Comparar_list[i]=Novaletra
+                    Palavra_Display_list[i]=palavra_secreta_list[i].upper()
+            Palavra_Comparar=''.join(Palavra_Comparar_list)
             Palavra_Display=''.join(Palavra_Display_list)
             
         
         
-        if Palavra_Display==Palavra_formatada: #checa se ganhou
+        if Palavra_Comparar==Palavra_formatada: #checa se ganhou
          print(f'Ganhou! A palavra secreta é {palavra_secreta.upper()}!')
          imprime_mensagem_vencedor()
          dic_jogadores[Nome_jogador].atualiza_vitoria()
@@ -313,3 +320,26 @@ while not Restart_jogo:
 
 for jogador in dic_jogadores.values():
     print(jogador)
+
+def vitorias_jogador(dicionario,jogador):
+    return dicionario[jogador].retorna_n_vitorias()
+
+def percentual_vitorias_jogador(dicionario,jogador):
+    return (dicionario[jogador].retorna_n_vitorias())/(dicionario[jogador].retorna_n_derrotas()+dicionario[jogador].retorna_n_vitorias())
+
+Ranking_Jogadores_Vitorias=[jogador for jogador in dic_jogadores]
+Ranking_Jogadores_Aproveitamento=[jogador for jogador in dic_jogadores]
+
+Ranking_Jogadores_Vitorias.sort(key=lambda x:percentual_vitorias_jogador(dic_jogadores,x),reverse=True)
+Ranking_Jogadores_Aproveitamento.sort(key=lambda x:percentual_vitorias_jogador(dic_jogadores,x),reverse=True)
+
+
+print()
+print('Ranking Por Número de Vitórias:')
+for i in range(len(Ranking_Jogadores_Vitorias)):
+    print(f'N° {i+1} - Jogador:{Ranking_Jogadores_Vitorias[i]} - Vitórias {vitorias_jogador(dic_jogadores,Ranking_Jogadores_Vitorias[i]):4.1f}')
+
+print()
+print('Ranking percentual de vitórias:')
+for i in range(len(Ranking_Jogadores_Aproveitamento)):
+    print(f'N° {i+1} - Jogador:{Ranking_Jogadores_Vitorias[i]} - Aproveitamento {100*percentual_vitorias_jogador(dic_jogadores,Ranking_Jogadores_Vitorias[i]):4.1f} %')
