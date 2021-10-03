3d3e6872762749d8953659f4e78f87f7882b3db2
rtmiura <rodrigotksh@gmail.com>
2021-10-02 22:08:59 -0300

    Update teste_leitura_sorteio_palavra.py
    Arquivo com testes do robo inteligente

--- teste_leitura_sorteio_palavra.py
+++ teste_leitura_sorteio_palavra.py
@@ -12,7 +12,7 @@ from unicodedata import normalize
 #     from doctest import testmod
 #     testmod()
 
-arquivo = open('Palavras_red.txt', 'r', encoding="utf-8")
+arquivo = open('lista_palavras.txt', 'r', encoding="utf-8")
 conteudo = arquivo.read()
     
 arquivo.close()
@@ -44,3 +44,15 @@ def tratamento(palavra):
 palavra_secreta=sorteio_palavra(conteudo_lista)
 teste=tratamento(palavra_secreta)
 
+Nova_lista=[]
+for i in range(26000):
+    Nova_lista.append(random.choice(conteudo_lista))
+
+Nova_lista.sort()
+Nova_lista_texto='\n'.join(Nova_lista)
+
+arquivo = open("Lista_Teste.txt", "w", encoding="utf-8")
+
+arquivo.write(Nova_lista_texto) 
+
+arquivo.close()
